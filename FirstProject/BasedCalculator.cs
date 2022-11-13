3e18b270e731e63cd6b335f2a13724fc7e82c7e6
Łukasz Skrzypek <lukasz.skrzypek95@gmail.com>
2022-11-13 19:13:04 +0100

    correction

--- FirstProject/BasedCalculator.cs
+++ FirstProject/BasedCalculator.cs
@@ -9,23 +9,9 @@ namespace FirstProject
 {
     internal class BasedCalculator
     {
-
-        public int Addiction(int x, int y)
-        {
-            return x + y;
-        }
-        public int Substraction(int x, int y)
-        {
-            return x - y;
-        }
-        public int Multiplication(int x, int y)
-        {
-            return x * y;
-        }
-        public int Division(int x, int y)
-        {
-            return x / y; 
-        }
-
+        public int Addiction(int number1, int number2) => (number1 + number2);
+        public int Substraction(int number1, int number2) => (number1 - number2);
+        public int Multiplication(int number1, int number2) => (number1 * number2);
+        public int Division(int number1, int number2) => (number1 / number2);
     }
 }
--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,20 +1,37 @@
 ﻿using FirstProject;
+using System;
+using System.ComponentModel;
 using System.ComponentModel.Design;
-
-Console.WriteLine("FirstProject");
-
-
-    BasedCalculator basedcalculator = new BasedCalculator();
-Console.WriteLine("Kalkulator podstawowy");
+BasedCalculator basedcalculator = new BasedCalculator();
 Console.WriteLine("Dodawanie - wybierz 1");
 Console.WriteLine("Odejmowanie - wybierz 2");
 Console.WriteLine("Mnożenie - wybierz 3");
 Console.WriteLine("Dzielenie - wybierz 4");
 int action = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Podaj liczby");
-    int x = Convert.ToInt32(Console.ReadLine());
-    int y = Convert.ToInt32(Console.ReadLine());
-if (action == 1)
+Console.WriteLine("Podaj liczby");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+switch(action)
+{
+    case 1:
+        Console.WriteLine("Dodawanie");
+        Console.WriteLine(basedcalculator.Addiction(number1, number2));
+        break;
+    case 2:
+        Console.WriteLine("Odejmowanie");
+        Console.WriteLine(basedcalculator.Substraction(number1, number2));
+        break;
+    case 3:
+        Console.WriteLine("Mnożenie");
+        Console.WriteLine(basedcalculator.Multiplication(number1, number2));
+        break;
+    case 4:
+        Console.WriteLine("Dzielenie");
+        Console.WriteLine(basedcalculator.Division(number1, number2));
+        break;
+
+}
+/*if (action == 1)
 {
     Console.WriteLine("Dodawanie");
     int sum = basedcalculator.Addiction(x, y);
@@ -38,3 +55,4 @@ else if (action == 4)
     int div = basedcalculator.Division(x, y);
     Console.WriteLine(div);
 }
+*/
