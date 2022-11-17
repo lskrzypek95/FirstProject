44d1618190b7b7cb028b504ec38a41d1c3553ed2
Łukasz Skrzypek <lukasz.skrzypek95@gmail.com>
2022-11-17 19:40:11 +0100

    Correction

---
+++ FirstProject/BaseAlgorithms.cs
@@ -0,0 +1,17 @@
+﻿
+namespace FirstProject
+{
+    internal class BaseAlgorithms
+    { 
+        public bool IsPrime(int value)
+        {
+            bool isPrime = true;
+            for (int i = 2; i < value; i++)
+            {
+                if (value % i == 0)
+                    isPrime = false;
+            }
+            return isPrime;  
+        }
+    }
+}
--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,17 +1,17 @@
 ﻿using FirstProject;
 using System;
 BaseCalculator basedcalculator = new BaseCalculator();
-PrimeNumber primeNumber = new PrimeNumber();
+BaseAlgorithms primeNumber = new BaseAlgorithms();
 Console.WriteLine("Dodawanie - wybierz 1");
 Console.WriteLine("Odejmowanie - wybierz 2");
 Console.WriteLine("Mnozenie - wybierz 3");
 Console.WriteLine("Dzielenie - wybierz 4");
 Console.WriteLine("Dodatkowe dzialania - wybierz 5");
-int action = Convert.ToInt32(Console.ReadLine());
+int baseAction = Convert.ToInt32(Console.ReadLine());
 int number1 = 0;
 int number2 = 0;
 
-switch (action)
+switch (baseAction)
 {
     case 1:
         Console.WriteLine("Podaj liczby");
@@ -44,14 +44,14 @@ switch (action)
     case 5:
         Console.WriteLine("Dodatkowe Dzialania");
         Console.WriteLine("Liczba Pierwsza - wybierz 1");
-        int baseAction = Convert.ToInt32(Console.ReadLine());
+        int baseAlgorithms = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Podaj liczbę");
-        int value1 = Convert.ToInt32(Console.ReadLine());
-        switch (baseAction)
+        int value = Convert.ToInt32(Console.ReadLine());
+        switch (baseAlgorithms)
         {
             case 1:
                 Console.WriteLine("Czy liczba jest liczba pierwsza?");
-                if (primeNumber.isPrime(value1))
+                if (primeNumber.IsPrime(value))
                 Console.WriteLine("Liczba jest liczba pierwsza");
                 else
                     Console.WriteLine("Liczba nie jest liczba pierwsza");
